b2980e2480a36109cc2bbcb89c0e56b70aacbc3a
Michael C. Thompson <mcthompson1981@gmail.com>
2025-10-23 16:42:11 -0400

    cleanup

--- lib/makeswift/client.ts
+++ lib/makeswift/client.ts
@@ -3,6 +3,7 @@ import { strict } from 'assert'
 
 import { runtime } from './runtime'
 
+//console.log('API Key:', process.env.MAKESWIFT_SITE_API_KEY)
 strict(process.env.MAKESWIFT_SITE_API_KEY, 'MAKESWIFT_SITE_API_KEY is required')
 
 export const client = new Makeswift(process.env.MAKESWIFT_SITE_API_KEY, {
